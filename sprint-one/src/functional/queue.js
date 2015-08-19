9c11fda5f0ed29680ecc8b0d8cc39d41cd944d7d
juaniiie <becjuana@gmail.com>
2015-08-19 15:34:24 -0700

    all tests passed in queue and stack for functional patter except for last

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,35 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    length = Object.keys(storage).length;
+    storage[length] = value;
   };
 
   someInstance.dequeue = function(){
+    length = Object.keys(storage).length;
+    var popped = storage[0];
+    delete storage[0];
+    return popped;
+    var arrKeys = [];
+    var arrVals = [];
+    for(var key in storage){
+      arrKeys.push(key);
+      arrVals.push(storage[key]);
+    }
+    for(var i = 0; i < arrKeys.length; i++){
+      arrKeys[i] = i;
+      storage[i] = arrVals[i];
+    }
   };
 
   someInstance.size = function(){
+    length = Object.keys(storage).length;
+    return length;
   };
 
   return someInstance;
