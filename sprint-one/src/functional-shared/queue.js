917bcf894973b33a5dd391ee011a38a9a9d2d67c
juaniiie <becjuana@gmail.com>
2015-08-19 16:42:30 -0700

    completed stack of functional share

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,45 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {
+		storage: {},
+		storageLength: 0,
+	};
+
+	someInstance = _.extend(someInstance, queueMethods);
+	return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+	enqueue: function(value){
+	    length = Object.keys(storage).length;
+	    storage[length] = value;
+  	},
+
+  	dequeue: function(){
+	    length = Object.keys(storage).length;
+	    var popped = storage[0];
+	    delete storage[0];
+	    return popped;
+	    var arrKeys = [];
+	    var arrVals = [];
+	    for(var key in storage){
+	      arrKeys.push(key);
+	      arrVals.push(storage[key]);
+	    }
+	    for(var i = 0; i < arrKeys.length; i++){
+	      arrKeys[i] = i;
+	      storage[i] = arrVals[i];
+	    }
+ 	},
+
+	size: function(){
+	    length = Object.keys(storage).length;
+	    return length;
+  	}
+
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+	var someInstance = {
+		storage: {},
+		storageLength: 0,
+	};
+
+	someInstance = _.extend(someInstance, stackMethods);
+	return someInstance;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+	push: function(value){
+	    this.storage[this.storageLength] = value;
+	    this.storageLength++;
+ 	 }, 
+ 	pop: function(){
+	    var popped = this.storage[this.storageLength - 1];
+	    delete this.storage[this.storageLength - 1];
+	    this.storageLength--;
+	    if (this.storageLength < 0){
+	    	this.storageLength = 0;
+	    }
+	    return popped;
+  	},
+  	size: function(){
+	    return this.storageLength;
+  	}
+};
