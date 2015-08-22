37de9b0f39e89059fe5dc86951e96279144d79a1
juaniiie <becjuana@gmail.com>
2015-08-22 11:42:16 -0700

    set structure pattern

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,35 @@
 var BinarySearchTree = function(value){
+	var node = {
+		value: value;
+	};
+	//this.left;
+	//this.right;
+	_.extend(node, methods);
+	return node;
 };
 
+var methods = {};
+
+methods.insert = function(value) {
+	var node = new BinarySearchTree(value);
+	if(this.value > value) {
+		this.left = node;
+	} else {
+		this.right = node;
+	}
+};
+
+methods.contains = function () {
+
+};
+
+methods.depthFirstLog = function () {
+	// _.each(this.arrOfNodes, function(node){
+	// 	var valueNode = node.value;
+	// 	cb(valueNode);
+	// });
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
